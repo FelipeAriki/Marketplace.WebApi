410fe719148f44eec10331b41de91c8e41fd310d
FelipeAriki <felipeariki3@gmail.com>
2024-04-08 20:57:48 -0300

    feat: Criando entidade produto.

--- Marketplace.Domain/Entities/Produto.cs
+++ Marketplace.Domain/Entities/Produto.cs
@@ -8,5 +8,14 @@ namespace Marketplace.Domain.Entities
 {
     public class Produto
     {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set;}
+        public long Preco { get; set;}
+        public int QuantidadeEstoque { get; set;}
+        public string? Categoria { get; set;}
+        public string? Fornecedor { get; set;}
+        public DateTime DataAdicaoInventario { get; set;}
+
     }
 }
